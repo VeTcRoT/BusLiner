dc99da5216cfde845690d34a9511ce4d1738b56e
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-11 13:45:54 +0300

    Did initial migration

---
+++ src/BusLiner/BusLiner.Persistence/Migrations/20230511101015_InitialMigration.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusLiner.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ArrivalPlaces",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Station = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArrivalPlaces", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeparturePlaces",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Station = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeparturePlaces", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rides",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TicketsAvailable = table.Column<int>(type: "int", nullable: false),
+                    DepartureTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ArrivalTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    DeparturePlaceId = table.Column<int>(type: "int", nullable: false),
+                    ArrivalPlaceId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rides", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rides_ArrivalPlaces_ArrivalPlaceId",
+                        column: x => x.ArrivalPlaceId,
+                        principalTable: "ArrivalPlaces",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rides_DeparturePlaces_DeparturePlaceId",
+                        column: x => x.DeparturePlaceId,
+                        principalTable: "DeparturePlaces",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rides_ArrivalPlaceId",
+                table: "Rides",
+                column: "ArrivalPlaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rides_DeparturePlaceId",
+                table: "Rides",
+                column: "DeparturePlaceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rides");
+
+            migrationBuilder.DropTable(
+                name: "ArrivalPlaces");
+
+            migrationBuilder.DropTable(
+                name: "DeparturePlaces");
+        }
+    }
+}
