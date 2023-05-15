ed8071ecbd0d05c134585be9c7d18618f72354bd
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-15 14:41:59 +0300

    Implemented entering with Google And Facebook

--- src/BusLiner/BusLiner.MVC/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ src/BusLiner/BusLiner.MVC/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -155,7 +155,7 @@ namespace BusLiner.MVC.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-
+                
                 var result = await _userManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
