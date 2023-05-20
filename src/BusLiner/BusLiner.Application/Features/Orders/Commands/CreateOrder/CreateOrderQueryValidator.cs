193163629722506244846a6bac53ff20d00593d1
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-20 12:51:15 +0300

    Translated validation errors

--- src/BusLiner/BusLiner.Application/Features/Orders/Commands/CreateOrder/CreateOrderQueryValidator.cs
+++ src/BusLiner/BusLiner.Application/Features/Orders/Commands/CreateOrder/CreateOrderQueryValidator.cs
@@ -8,38 +8,38 @@ namespace BusLiner.Application.Features.Orders.Commands.CreateOrder
         public CreateOrderQueryValidator()
         {
             RuleFor(o => o.FirstName)
-                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotEmpty().WithMessage("Ім'я є необхідним.")
                 .NotNull()
-                .MaximumLength(30).WithMessage("{PropertyName} should not exceed 30 characters.")
-                .MinimumLength(3).WithMessage("{PropertyName} length should be greater or equals to 3.");
+                .MaximumLength(30).WithMessage("Довжина ім'я має бути не більшою 30 символів.")
+                .MinimumLength(3).WithMessage("Довжина ім'я має бути не меншою 3 символів.");
 
             RuleFor(o => o.LastName)
-                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotEmpty().WithMessage("Прізвище є необхідним.")
                 .NotNull()
-                .MaximumLength(30).WithMessage("{PropertyName} should not exceed 30 characters.")
-                .MinimumLength(3).WithMessage("{PropertyName} length should be greater or equals to 3.");
+                .MaximumLength(30).WithMessage("Довжина прізвища має бути не більшою 30 символів.")
+                .MinimumLength(3).WithMessage("Довжина прізвища має бути не меншою 3 символів.");
 
             RuleFor(o => o.Email)
-                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotEmpty().WithMessage("Email є необхідним.")
                 .NotNull()
-                .EmailAddress().WithMessage("{PropertyName} should be a valid email.")
-                .MaximumLength(50).WithMessage("{PropertyName} should not exceed 50 characters.");
+                .EmailAddress().WithMessage("Email введено в неправильному форматі.")
+                .MaximumLength(50).WithMessage("Довжина email має бути не більшою 50 символів.");
 
             RuleFor(o => o.Phone)
-                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotEmpty().WithMessage("Телефон є необхідним.")
                 .NotNull()
-                .Matches(@"^\d{10}$").WithMessage("{PropertyName} should be a valid phone number.");
+                .Matches(@"^\d{10}$").WithMessage("Телефон введено в неправильному форматі.");
 
             RuleFor(o => o.TicketsOrdered)
-                .NotEmpty().WithMessage("{PropertyName} is required.")
-                .GreaterThanOrEqualTo(1).WithMessage("{PropertyName} should be greater or equals to 1.")
-                .LessThanOrEqualTo(5).WithMessage("{PropertyName} should be less or equals to 5.");
+                .NotEmpty().WithMessage("К-сть замовлених квитків є необхідним.")
+                .GreaterThanOrEqualTo(1).WithMessage("Мінімальне значення к-сті замовлених квитків є 1.")
+                .LessThanOrEqualTo(5).WithMessage("Не можна замовити більше 5 квитків.");
 
             //RuleFor(o => o.AdditionalBaggage)
             //    .NotEmpty().WithMessage("{PropertyName} is required.");
 
             RuleFor(o => o)
-                .Must(IsAdditionalBaggageNumberValid);
+                .Must(IsAdditionalBaggageNumberValid).WithMessage("К-сть додаткового багажу немає перевищувати к-сть білетів помножену на 2.");
         }
         private bool IsAdditionalBaggageNumberValid(CreateOrderQuery order)
         {
--- src/BusLiner/BusLiner.Application/Features/Rides/Queries/GetRidesByOptions/GetRidesByOptionsQueryValidator.cs
+++ src/BusLiner/BusLiner.Application/Features/Rides/Queries/GetRidesByOptions/GetRidesByOptionsQueryValidator.cs
@@ -7,21 +7,21 @@ namespace BusLiner.Application.Features.Rides.Queries.GetRidesByOptions
         public GetRidesByOptionsQueryValidator()
         {
             RuleFor(o => o.From)
-                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotEmpty().WithMessage("Звідки є необхідним.")
                 .NotNull()
-                .MaximumLength(30).WithMessage("{PropertyName} should not exceed 30 characters.")
-                .MinimumLength(4).WithMessage("{PropertyName} length should be greater or equals to 4 characters.");
+                .MaximumLength(30).WithMessage("Довжина звідки має бути не більшою 30 символів.")
+                .MinimumLength(4).WithMessage("Довжина звідки має бути не більшою 4 символів.");
 
             RuleFor(o => o.To)
-                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotEmpty().WithMessage("Куди є необхідним.")
                 .NotNull()
-                .MaximumLength(30).WithMessage("{PropertyName} should not exceed 30 characters.")
-                .MinimumLength(4).WithMessage("{PropertyName} length should be greater or equals to 4 characters.");
+                .MaximumLength(30).WithMessage("Довжина куди має бути не більшою 30 символів.")
+                .MinimumLength(4).WithMessage("Довжина куди має бути не більшою 4 символів.");
 
             RuleFor(u => u.DepartureDate)
-                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotEmpty().WithMessage("Дата відправлення є необхідною.")
                 .NotNull()
-                .Must(BeAValidDate).WithMessage("{PropertyName} enetered in wrong format.");
+                .Must(BeAValidDate).WithMessage("Дата відправлення введена в неправильному форматі.");
         }
         private bool BeAValidDate(DateTime date)
         {
